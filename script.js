9df494411e6cf3d4988971031bac3fb930805802
Ivett Ördög <devillmeister@gmail.com>
2020-03-20 14:44:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 import { ApplicationApi, ApplicationUI, EditorPlugin, 
-	SidebarUI, UIRoot, MenuItemKind,MenuId } from "prezi_plugin_api";
+	SidebarUI, UIRoot, MenuItemKind, MenuId, ListProgress,
+	ListItemLayout, ListItemType, ImageArgs } from "prezi_plugin_api";
+
+type VideoThumbnail = {
+
+}
 
 type UiState = {
-	// Define the state of your plugin here
+	searchString: string
+	resultList: VideoThumbnail[]
+};
+
+type VideoFile = {
+	id: number
+	quality: string
+	file_type: string
+	width: number
+	height: number
+	link: string
+}
+
+type Video = {
+	url: string,
+	image: string
+	video_files: VideoFile[]
 };
 
 class Main implements EditorPlugin {
 	init(applicationApi: ApplicationApi) {
 		const uiRoot = applicationApi.declareUI<UiState>({
-				// initial state comes here
+				searchString: "",
+				resultList: []
 			},
 			(state, ui) => {
 				this.createMenuItem(ui, state, uiRoot);
@@ -16,44 +38,82 @@ class Main implements EditorPlugin {
 			}
 		);
 		// open the sidebar automatically
-		setTimeout(() => {uiRoot.sidebar.open("mySidebarId");}, 500);
+		setTimeout(() => {uiRoot.sidebar.open("pexelsSidebar");}, 500);
 
 	}
 
 	createMenuItem(ui: ApplicationUI, state: UiState, uiRoot: UIRoot<UiState>) {
 		ui.createMenuItem({
 			kind: MenuItemKind.dropdown,
-			id: "myMenuItem",
+			id: "pexelsMenuItem",
 			parent: MenuId.Lab,
 			weight: 3,
-			title: "Open my sidebar",
+			title: "Pexels Video",
 			onClick: () => {
-				uiRoot.sidebar.open("mySidebarId");
+				uiRoot.sidebar.open("pexelsSidebar");
 			},
 		});
 	}
 
 	createSidebar(ui: ApplicationUI, state: UiState, uiRoot: UIRoot<UiState>) {
 		return ui.createSidebar(sidebarUi => ({
-			id: "mySidebarId",
+			id: "pexelsSidebar",
 			root: {
-				title: "My sidebar",
+				title: "Insert Pexels Video",
 				content: [
-					this.createNameInput(sidebarUi, state, uiRoot),
+					this.createSearchField(sidebarUi, state, uiRoot),
+					this.createResultList(sidebarUi, state, uiRoot)
 				]
 			}
 		}));
 	}
 
-		createNameInput(sidebarUi: SidebarUI, state: UiState, uiRoot: UIRoot<UiState>) {
-		return sidebarUi.inputField({
+	createSearchField(sidebarUi: SidebarUI, state: UiState, uiRoot: UIRoot<UiState>) {
+		return sidebarUi.searchField({
 			id: "searchText",
-			placeholderText: "Search...",
-			value: "",
-			onValueChange: (_, v) => {
-				uiRoot.setState({name: v});
-				return true;
+			value: state.searchString,
+			onValueChange: (ctx, value) => {
+				uiRoot.setState({searchString: value});
+			},
+			onSearchButtonClick: (ctx) => {
+				let encodedSearch = state.searchString;
+				fetch(
+					`https://api.pexels.com/videos/search?query=${encodedSearch}&per_page=15&page=1`,
+					{
+						headers: {
+							Authorization: <API_CODE>
+						}
+					}
+				).then(res => res.json()).then(json => {
+					uiRoot.setState({ resultList: json.videos.map( (video:Video) => { 
+						return {
+							url: video.image,
+							disabled: false,
+							onClick: () => {/*...*/},
+							hoverContent: {"type":"image","url":"video.image"},
+							info: {
+								"url":"http://prezi.com",
+								"icon":"icon-small-user",
+								"text":"Luke Skywalker",
+								"tooltip":"By Luke Skywalker"
+							},
+						}
+						})
+					});
+				})
 			}
 		});
+	}
+
+	createResultList(sidebarUi: SidebarUI, state: UiState, uiRoot: UIRoot<UiState>) {
+		return sidebarUi.simpleList(itemUi => ({
+			items: state.resultList.map((video:ImageArgs) => itemUi.image(video)),
+			itemLayout: ListItemLayout.fixed,
+			itemType: ListItemType.large,
+			showTitle: false,
+			flexGrow: 1,
+			progress: ListProgress.finished,
+			activeId: null,
+		}))
 	}
 }
